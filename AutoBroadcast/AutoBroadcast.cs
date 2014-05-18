a3c6885885eb1c2f465a80aaf7ca7dcfee049ee8
CAWCAWCAW <specialops0@hotmail.com>
2014-05-18 02:51:49 -0700

    API 1.16
    API 1.16

--- AutoBroadcast/AutoBroadcast.cs
+++ AutoBroadcast/AutoBroadcast.cs
@@ -8,7 +8,7 @@ using TShockAPI;
 
 namespace AutoBroadcast
 {
-	[ApiVersion(1, 15)]
+	[ApiVersion(1, 16)]
 	public class AutoBroadcast : TerrariaPlugin
 	{
 		public override string Name { get { return "AutoBroadcast"; } }
